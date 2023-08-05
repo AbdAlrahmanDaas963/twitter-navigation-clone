db713209daced0a300533f7d17488798f9c3a505
Abdalrahman <abdalrahmandaas963@gmail.com>
2023-08-05 12:13:12 +0300

    first

---
+++ screens/tabScreens/Settings.js
@@ -0,0 +1,9 @@
+import { Button, Text, View } from "react-native";
+
+export default function Settings() {
+  return (
+    <View>
+      <Text style={{ color: "white" }}>Settings</Text>
+    </View>
+  );
+}
